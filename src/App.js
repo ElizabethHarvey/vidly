c79a3ddbac053ce130d8bb1df1890a00fff69274
Elizabeth Harvey <lizzy220188@gmail.com>
2022-06-23 16:56:59 -0400

    Refs

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ class App extends Component {
           <Switch>
             <Route path="/LoginForm" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound}></Route>
-            <Redirect path="/" exact to="/movies" />
-            <Redirect path="/not-found" />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
           </Switch>
         </main>
       </React.Fragment>
