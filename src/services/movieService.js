06f19552dba1b2124b8657990e189d106051ee08
Elizabeth Harvey <lizzy220188@gmail.com>
2022-07-24 14:49:28 -0400

    fixed movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -16,7 +16,7 @@ export function getMovie(movieId) {
 }
 
 export function saveMovie(movie) {
-  if (movie._id) {
+  if (movie._id) { 
     const body = { ...movie };
     delete body._id;
     return http.put(movieUrl(movie._id), body);
